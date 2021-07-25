aedccc4fa8ab3267e58483e9e16874de4df59045
kctrnn <kctrnn@gmail.com>
2021-07-25 22:18:26 +0700

    Setup project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export type AppDispatch = typeof store.dispatch;
